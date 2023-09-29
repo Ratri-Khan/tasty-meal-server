026cc8e89b4a32379bc1b16ab2b3d8e8de9d6032
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-09-29 21:02:37 +0600

    details send

--- index.js
+++ index.js
@@ -7,16 +7,34 @@ app.use(cors())
 const bannerInfo = require('./data/banner.json');
 const chefInfo = require('./data/chefInfo.json');
 const processingInfo = require('./data/FoodProcessing.json');
+const recipeInfo = require('./data/recipeDetails.json');
 
-app.get(('/bannerInfo') , (req,res) =>{
-   res.send(bannerInfo);
+app.get(('/bannerInfo'), (req, res) => {
+    res.send(bannerInfo);
 });
-app.get(('/chefInfo') , (req,res) =>{
+app.get(('/chefInfo'), (req, res) => {
     res.send(chefInfo);
- })
- app.get(('/processingInfo') , (req,res) =>{
+});
+app.get(('/processingInfo'), (req, res) => {
     res.send(processingInfo);
- })
+});
+app.get(('/recipeInfo'), (req, res) => {
+    res.send(recipeInfo);
+});
+app.get('/recipeInfo/:id', (req,res) =>{
+    const id = req.params.id;
+    console.log(id);
+    const selectedInfo = recipeInfo.find(i => parseInt(i.id) === id);
+    res.send(selectedInfo);
+  })
+  app.get('/chefInfo/:id',(req,res) =>{
+    const id = parseInt(req.params.id);
+    console.log(id);
+    const informationChefs = recipeInfo.filter(i => parseInt(i.id) === id);
+    res.send(informationChefs);
+  })
+  
+
 
 
 
@@ -25,9 +43,9 @@ app.get(('/chefInfo') , (req,res) =>{
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
